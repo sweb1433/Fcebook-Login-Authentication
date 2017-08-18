f0d95c489d7aa7440a192039127c7f904e07d311
sweb1433 <sweb1433@gmail.com>
2017-08-18 14:44:15 +0530

    first commit

---
+++ config/auth.js
@@ -0,0 +1,7 @@
+module.exports = {
+	'facebookAuth': {
+		'clientID': '537660239636336',
+		'clientSecret': '944cd441fe65c408f489f1304afc5971',
+		'callbackURL': 'http://localhost:8080/auth/facebook/callback'
+	}
+}
